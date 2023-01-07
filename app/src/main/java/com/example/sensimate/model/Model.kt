e03750c504298afa9ac63b209d03ddb68b9ab40e
Jackie <Jackie_Trinh@hotmail.com>
2023-01-07 16:15:21 +0100

    added styling to event items

--- app/src/main/java/com/example/sensimate/model/Model.kt
+++ app/src/main/java/com/example/sensimate/model/Model.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -57,26 +59,25 @@ fun EventCardSelection(navController: NavController, events: List<com.example.se
 //EventItem - with clickable event to next page (currently survey)
 @Composable
 fun EventItem(navController: NavController, event: Event) {
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(MaterialTheme.colors.surface)
-            .padding(10.dp, 5.dp)
-            .clickable {
-                navController.popBackStack()
-            }
-            .clickable {
-                navController.navigate(route = NavRoutes.EventPage.route)
-            }
-    ) {
+    Card(elevation = 8.dp, shape = RoundedCornerShape(20.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colors.surface)
+                .padding(10.dp, 5.dp)
+                .clickable {
+                    navController.navigate(route = NavRoutes.EventPage.route)
+                }
+        ) {
             Icon(
                 imageVector = Icons.Filled.Person,
                 contentDescription = "profile",
                 tint = Color.Blue,
                 modifier = Modifier.size(150.dp)
             )
-        EventItemDetail(item = event)
+            EventItemDetail(item = event)
+        }
     }
 }
 
