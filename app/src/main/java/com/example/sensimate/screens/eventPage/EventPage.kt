a73521bc8d727f4b3703e40591cfa27fd550901b
Biskopstoe <s215788@dtu.dk>
2023-01-11 02:07:28 +0100

    Added image back into eventPage.kt, still figuring out howt to make it full width, and make the swipe lockable, but i figure that it's defined with the threshold of the swipable modifier.

--- app/src/main/java/com/example/sensimate/screens/eventPage/EventPage.kt
+++ app/src/main/java/com/example/sensimate/screens/eventPage/EventPage.kt
@@ -1,27 +1,36 @@
 package com.example.sensimate.screens.eventPage
 
+import android.content.ClipData.Item
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.sensimate.R
 import com.example.sensimate.navigation.BottomBarScreen
-import com.example.sensimate.screens.discover.DiscoverViewModel
 import com.example.sensimate.screens.eventManager.EventManagerViewModel
+import java.time.format.TextStyle
 
 @Composable
 fun EventPage(
@@ -58,40 +67,76 @@ fun EventPage(
                }
                Row(modifier = Modifier
                    .fillMaxHeight()
-                   .clickable {  navController.navigate("${BottomBarScreen.ManageEventPage.route}/${eventId}") }
+                   .clickable { navController.navigate("${BottomBarScreen.ManageEventPage.route}/${eventId}") }
                ){
                    Text(
                        text = "Edit Event",
                    )
                }
-
-
-
-
            } 
         }
-        item {
-            Image(painter = painterResource(id = R.drawable.bar_image),
-                contentDescription = "event image",
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(180.dp),
-            contentScale = ContentScale.Crop)
-        }
-        item {
-            Text(text = viewModel.event.id.toString())
-        }
-        item {
-            Text(text = viewModel.event.title)
-        }
+
+        item { Text(text = viewModel.event.title, fontSize = 28.sp, fontWeight = FontWeight.Bold) }
+
+        item { Swipe() }
+
         item {
             Text(text = viewModel.event.date)
-        }
-        item {
             Text(text = viewModel.event.address)
-        }
-        item {
+            Text(text = "Beskrivelse", fontSize = 16.sp, fontWeight = FontWeight.Bold)
             Text(text = viewModel.event.description)
+
+
         }
     }
+}
+
+@Preview
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun Swipe() {
+    LazyRow(
+        modifier = Modifier
+            .swipeable(
+                state = rememberSwipeableState(0),
+                anchors = mapOf(0f to 0, 1f to 1),
+                orientation = Orientation.Horizontal,
+                thresholds = { _, _ -> FractionalThreshold(0f) }
+            )
+            .background(MaterialTheme.colors.background),
+        contentPadding = PaddingValues(16.dp,4.dp),
+        horizontalArrangement = Arrangement.spacedBy(24.dp)
+
+    ) {
+        items(2)
+        { item -> ImageCard(painterResource(id = R.drawable.bar_image), "", "")
+
+        }
+    }
+}
+
+@Composable
+private fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        elevation = 4.dp,
+        border = BorderStroke(1.dp, color = Color.Black),
+        shape = RoundedCornerShape(10.dp),
+    ) {
+        Row(modifier = Modifier
+            .height(200.dp),
+            horizontalArrangement = Arrangement.Center){
+                Image(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    contentScale = ContentScale.Crop)
+
+        }
+    }
+
 }
