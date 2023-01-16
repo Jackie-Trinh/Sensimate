c45127706e4fd1373428688f2d1f4bf193113e3e
Biskopstoe <s215788@dtu.dk>
2023-01-16 01:03:03 +0100

    Fiddling with pictures

--- app/src/main/java/com/example/sensimate/screens/eventPage/EventPage.kt
+++ app/src/main/java/com/example/sensimate/screens/eventPage/EventPage.kt
@@ -1,25 +1,38 @@
 package com.example.sensimate.screens.eventPage
 
 import android.content.ClipData.Item
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import android.graphics.ColorSpace.Model
+import android.net.Uri
+import android.widget.StackView
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.FlingBehavior
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.ScrollableDefaults
+import androidx.compose.foundation.gestures.snapping.SnapFlingBehavior
+import androidx.compose.foundation.gestures.snapping.SnapLayoutInfoProvider
+import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,10 +40,16 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.request.ImageResult
 import com.example.sensimate.R
 import com.example.sensimate.navigation.BottomBarScreen
 import com.example.sensimate.screens.eventManager.EventManagerViewModel
+import java.net.URI
 import java.time.format.TextStyle
+import java.util.Stack
+import kotlin.time.Duration.Companion.seconds
 
 @Composable
 fun EventPage(
@@ -73,29 +92,60 @@ fun EventPage(
                        text = "Edit Event",
                    )
                }
-           } 
+           }
         }
 
-        item { Text(text = viewModel.event.title, fontSize = 28.sp, fontWeight = FontWeight.Bold) }
+        item{
+            Box {
+                Swipe()
+                Swipe()
+            }
+        }
+        item {
+            ImageCard()
+        }
 
-        item { Swipe() }
 
         item {
-            Text(text = viewModel.event.date)
-            Text(text = viewModel.event.address)
-            Text(text = "Beskrivelse", fontSize = 16.sp, fontWeight = FontWeight.Bold)
-            Text(text = viewModel.event.description)
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp)
+                //horizontalAlignment = Alignment.,
+            ) {
+                    Column(
+                        modifier = Modifier,
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(
+                            text = viewModel.event.title,
+                            fontSize = 28.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(text = viewModel.event.date)
+                        //Text(text = viewModel.event.address)
+                        Text(text = "Beskrivelse", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+                        Text(text = viewModel.event.description)
 
-
+                }
+            }
         }
     }
 }
 
-@Preview
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
 @Composable
-private fun Swipe() {
+private fun Swipe(
+) {
+    val state = rememberLazyListState()
+    val snappingLayout = remember(state) { SnapLayoutInfoProvider(state) }
+    val flingBehavior = rememberSnapFlingBehavior(snappingLayout)
     LazyRow(
+        flingBehavior = flingBehavior,
+        userScrollEnabled = true,
+        contentPadding = PaddingValues(horizontal = 0.dp, vertical = 0.dp),
+        horizontalArrangement = Arrangement.spacedBy(24.dp),
         modifier = Modifier
             .swipeable(
                 state = rememberSwipeableState(0),
@@ -103,39 +153,39 @@ private fun Swipe() {
                 orientation = Orientation.Horizontal,
                 thresholds = { _, _ -> FractionalThreshold(0f) }
             )
-            .background(MaterialTheme.colors.background),
-        contentPadding = PaddingValues(16.dp,4.dp),
-        horizontalArrangement = Arrangement.spacedBy(24.dp)
+            .background(MaterialTheme.colors.background)
+            .fillMaxSize(),
+
+        verticalAlignment = Alignment.CenterVertically
 
     ) {
-        items(2)
-        { item -> ImageCard(painterResource(id = R.drawable.bar_image), "", "")
-
-        }
+        item { ImageCard() }
     }
 }
 
+
 @Composable
 private fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
 ) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        elevation = 4.dp,
-        border = BorderStroke(1.dp, color = Color.Black),
-        shape = RoundedCornerShape(10.dp),
-    ) {
-        Row(modifier = Modifier
-            .height(200.dp),
-            horizontalArrangement = Arrangement.Center){
-                Image(
-                    painter = painter,
-                    contentDescription = contentDescription,
-                    contentScale = ContentScale.Crop)
+    Row(modifier = Modifier.fillMaxSize(),
 
+    ) {
+        Card(modifier = Modifier.fillMaxSize(),
+            shape = RoundedCornerShape(10.dp),
+            elevation = 1.dp)
+        { Box (modifier = Modifier,
+        contentAlignment = Alignment.Center) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(R.drawable.bar_image)
+                    .crossfade(true)
+                    .build(),
+                placeholder = painterResource(R.drawable.bar_image),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxWidth()
+            )
+            }
         }
     }
 
